c602f2bda7c138ee13d74607e815f1b790a1ad4d
Trevor McAlister <133400240+TrevorM56@users.noreply.github.com>
2024-03-05 11:03:56 -0600

    Update and rename SQL_JOINs.sql to SQL_Tutorial/SQL_JOINs.sql

--- SQL_Tutorial/SQL_JOINs.sql
+++ SQL_Tutorial/SQL_JOINs.sql
@@ -2,20 +2,20 @@
 	UNION, UNION ALL
 */
 
---CREATE TABLE WareHouseEmployeeDemographics (
---	Employee_id int, 
---	First_name varchar(50), 
---	Last_name varchar(50), 
---	Age int, 
---	Gender varchar(50)
---)
+CREATE TABLE WareHouseEmployeeDemographics (
+	Employee_id int, 
+	First_name varchar(50), 
+	Last_name varchar(50), 
+	Age int, 
+	Gender varchar(50)
+)
 
---INSERT INTO WareHouseEmployeeDemographics 
---VALUES
---	(1013, 'Darryl', 'Philben', NULL, 'Male'),
---	(1050, 'Roy', 'Anderson', 31, 'Male'),
---	(1051, 'Hidetoshi', 'Hasagawa', 40, 'Male'),
---	(1052, 'Val', 'Johnson', 31, 'Female')
+INSERT INTO WareHouseEmployeeDemographics 
+VALUES
+	(1013, 'Darryl', 'Philben', NULL, 'Male'),
+	(1050, 'Roy', 'Anderson', 31, 'Male'),
+	(1051, 'Hidetoshi', 'Hasagawa', 40, 'Male'),
+	(1052, 'Val', 'Johnson', 31, 'Female')
 
 SELECT
 	*
